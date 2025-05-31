8a228122395db73aff5aeccfeefe62bbe8506019
Ayca Dogan <dogan.aycaa@gmail.com>
2025-05-31 10:01:15 +0300

    Business Logic Layer katmaninda Ogrenci bolumu eklendi

---
+++ BusinessLogicLayer/OgrenciBLL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using EntityLayer;
+
+namespace BusinessLogicLayer
+{
+    public class OgrenciBLL
+    {
+        public static int OgrenciEkleBLL(Ogrenci ogr)
+        {
+            if (ogr.OgrAd!=null && ogr.OgrSoyad!=null && ogr.OgrNumara!=null && ogr.OgrSifre!=null && ogr.OgrFotograf!=null)
+            {
+                return OgrenciDAL.OgrenciEkle(ogr);
+            }
+            return -1;
+        }
+    }
+}
